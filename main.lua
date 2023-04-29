a92e4e0e48ed48f8a048b856c3fc14463e5d0e98
brennop <brennopcordeiro@gmail.com>
2023-04-29 15:38:46 -0300

    add wip cep

--- main.lua
+++ main.lua
@@ -2,9 +2,14 @@ local server = require "server"
 
 local cpf = require "lib.cpf"
 local cnpj = require "lib.cnpj"
+local cep = require "lib.cep"
+
+-- set random seed
+math.randomseed(os.time())
 
 server
   .get("/cpf", cpf)
   .get("/cnpj", cnpj)
+  .get("/cep", cep)
   .listen()
 
